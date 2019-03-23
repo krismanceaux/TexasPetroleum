feef41add08c3889ae1781d8f7508642e66639fd
Kristopher Manceaux <krismanceaux@hotmail.com>
2019-03-23 02:28:14 -0500

    Recommit. View previous log

---
+++ TexasPetroleum/TexasPetroleum/Controllers/QuoteController.cs
@@ -0,0 +1,44 @@
+﻿using TexasPetroleum.DAL;
+using TexasPetroleum.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TexasPetroleum;
+
+namespace FuelRatePredictor.Controllers
+{
+    public class QuoteController : Controller
+    {
+        // GET: Quote
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public ActionResult QuoteHistory()
+        {
+            QuoteContext dbContext = new QuoteContext();
+            List<Client> clients = dbContext.Clients.ToList();
+            dbContext = new QuoteContext();
+            clients = dbContext.Clients.ToList();
+            List<FuelQuote> history = clients.First().FuelQuotes.ToList();
+            return View(history);
+        }
+
+      
+        public ActionResult SubmitSuccess()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FuelQuote model)
+        {
+            return View();
+        }
+    }
+}
