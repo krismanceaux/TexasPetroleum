f8da8a8ebe0f26145144589bda1114916fa48f86
brizzetto <bobbyrizzetto@gmail.com>
2019-03-23 18:21:55 -0500

    -Add constants and enums -Update QuoteController and Profile views

---
+++ TexasPetroleum/TexasPetroleum/Constants/QuoteConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TexasPetroleum.Constants
+{
+    public class QuoteConstants
+    {
+        public const double DESIRED_PROFIT_MARGIN = .25;
+    }
+}
---
+++ TexasPetroleum/TexasPetroleum/Enums/PricingEnums.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TexasPetroleum.Enums
+{
+    public class PricingEnums
+    {
+        public enum Seasons
+        {
+            [Display(Name="Summer")]
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            [Display(Name="Winter")]
+            September,
+            October,
+            November,
+            December,
+            January,
+            February
+        }
+    }
+}
