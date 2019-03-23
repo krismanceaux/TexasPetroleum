77a4182a11cafd5cb0dcbe33b521114702966500
brizzetto <bobbyrizzetto@gmail.com>
2019-03-23 03:31:52 -0500

    -Combine Login/Registration controllers into AccountController -Add AccountVM in place of LoginVM -Add Views for Login and Register

---
+++ TexasPetroleum/TexasPetroleum/Controllers/AccountController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TexasPetroleum.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(AccountVM user)
+        {
+            if (ModelState.IsValid)
+            {
+                var context = new QuoteContext();
+
+                //Temp while DB is down
+                //return Redirect("/Home/UserHub");
+
+                if (false)   //context.Clients.Any(x => x.Username == user.Username && x.Password == user.Password)
+                {
+                    //redirect to UserHub
+                    return Redirect("/Home/UserHub");
+                }
+                else
+                {
+                    // display error
+                    ModelState.AddModelError(string.Empty, "Invalid Username or Password");
+                    return View(user);
+                }
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            //Displays Registration Page
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(AccountVM user)
+        {
+            if(ModelState.IsValid)
+            {
+                using (var context = new QuoteContext())
+                {
+                    //check database for existing username
+                    if (context.Clients.Any(x => x.Username == user.Username))
+                    {
+                        ModelState.AddModelError(string.Empty, "Username already exists");
+                        return View(user);
+                    }
+                    else
+                    {
+                        return Redirect("/Home/Login");
+                    }
+                }
+            }
+
+            //Redirect to Login Page if model state valid
+            return View();
+        }
+    }
+}
--- TexasPetroleum/TexasPetroleum/ViewModels/AccountVM.cs
+++ TexasPetroleum/TexasPetroleum/ViewModels/AccountVM.cs
@@ -6,12 +6,14 @@ using System.Web;
 
 namespace TexasPetroleum
 {
-    public class LoginVM
+    public class AccountVM
     {
         [Required(ErrorMessage ="Username is required")]
         public string Username { get; set; }
 
         [Required(ErrorMessage ="Password is required")]
+        [MinLength(7, ErrorMessage ="Password must be at least 7 characters long")]
+        [RegularExpression("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$", ErrorMessage ="Password must contain at least one special character, one number, and letter")]
         public string Password { get; set; }
     }
 }
