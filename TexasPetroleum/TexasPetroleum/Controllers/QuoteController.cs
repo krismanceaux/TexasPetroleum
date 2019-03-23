210021c846258e352b59940db0ca1295f3469d60
brizzetto <bobbyrizzetto@gmail.com>
2019-03-23 02:19:36 -0500

    Add QuoteController

---
+++ TexasPetroleum/TexasPetroleum/Controllers/QuoteController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TexasPetroleum.Models;
+
+namespace TexasPetroleum.Controllers
+{
+    public class QuoteController : Controller
+    {
+        // GET: Quote
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult QuoteHistory()
+        {
+            QuoteContext dbContext = new QuoteContext();
+            List<Client> clients = dbContext.Clients.ToList();
+            dbContext = new QuoteContext();
+            clients = dbContext.Clients.ToList();
+            List<FuelQuote> history = clients.First().FuelQuotes.ToList();
+            return View(history);
+        }
+
+        [Authorize]
+        public ActionResult SubmitSuccess()
+        {
+            return View();
+        }
+    }
+}
