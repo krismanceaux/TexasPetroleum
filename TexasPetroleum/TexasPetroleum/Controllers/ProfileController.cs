e31886a57a6cf92db857ee1c7d872b98367ea88d
brizzetto <bobbyrizzetto@gmail.com>
2019-03-23 01:48:17 -0500

    Add Enums and Profile Controller/Views

---
+++ TexasPetroleum/TexasPetroleum/Controllers/ProfileController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TexasPetroleum.ViewModels;
+
+namespace TexasPetroleum.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            //var context = new QuoteContext();
+            //var client = context.Clients.SingleOrDefault(x => x.ClientId == )
+
+            //// If no client data exists, create new client to pass to view
+            //if (currentUser.Client == null)
+            //{
+            //    currentUser.Client = new Client();
+            //}
+
+            //ProfileViewModel vm = new ProfileViewModel()
+            //{
+            //    Name = currentUser.Client.Name,
+            //    Address = currentUser.Client.Address
+            //};
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ProfileVM model)
+        {
+
+            //var context = new ApplicationDbContext();
+            //var currentUserID = System.Web.HttpContext.Current.User.Identity.GetUserId();
+            //var currentUser = context.Users.Single(x => x.Id == currentUserID);
+            //var client = currentUser.Client;
+
+            //if (client == null)
+            //{
+            //    client = new Client();
+            //    client.ApplicationUser = currentUser;
+            //}
+
+            //client.Name = model.Name;
+            //client.Address.AddressLine1 = model.Address.AddressLine1;
+            //client.Address.AddressLine2 = model.Address.AddressLine2;
+            //client.Address.City = model.Address.City;
+
+            //var stateStringVal = model.StateOption.ToString();
+
+            //client.Address.State = stateStringVal;
+            //client.Address.ZipCode = model.Address.ZipCode;
+
+            //context.Clients.Add(client);
+            //context.Users.Attach(client.ApplicationUser);
+
+            //context.SaveChanges();
+
+            return View();
+        }
+    }
+}
--- TexasPetroleum/TexasPetroleum/ViewModels/ProfileVM.cs
+++ TexasPetroleum/TexasPetroleum/ViewModels/ProfileVM.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using static TexasPetroleum.Enums.DisplayEnums;
 
 namespace TexasPetroleum.ViewModels
 {
