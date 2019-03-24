35bb2118251899989a06b923d76ee36dce909d02
Kristopher Manceaux <krismanceaux@hotmail.com>
2019-03-24 02:31:35 -0500

    Tweaked Bobby's code to get the Profile fields to populate from the database. Made te same change to the Quote controller

--- TexasPetroleum/TexasPetroleum/DAL/QuoteContext.cs
+++ TexasPetroleum/TexasPetroleum/DAL/QuoteContext.cs
@@ -15,18 +15,5 @@
         public virtual DbSet<Address> Addresses { get; set; }
         public virtual DbSet<Client> Clients { get; set; }
         public virtual DbSet<FuelQuote> FuelQuotes { get; set; }
-
-        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<Client>()
-        //        .HasOptional(e => e.Address)
-        //        .WithRequired(e => e.Client);
-
-        //    modelBuilder.Entity<Client>()
-        //        .HasMany(e => e.FuelQuotes)
-        //        .WithOptional(e => e.Client)
-        //        .HasForeignKey(e => e.Client_ClientID)
-        //        .WillCascadeOnDelete();
-        //}
     }
 }
