826cef2097dfa6bc3032a06910e5d6c9aca8f6af
Hayden Brehm <thorrbobrehm@gmail.com>
2019-03-23 19:45:16 -0500

    added validations to profile and remade profile's edit.cshtml file

--- TexasPetroleum/TexasPetroleum/ViewModels/ProfileVM.cs
+++ TexasPetroleum/TexasPetroleum/ViewModels/ProfileVM.cs
@@ -11,13 +11,13 @@ namespace TexasPetroleum.ViewModels
     public class ProfileVM
     {
         [Display(Name = "Name")]
+        [StringLength(50, ErrorMessage = "Must be less than 50 characters")]
         [Required(ErrorMessage ="Name is required")]
-        [StringLength(50,ErrorMessage = "Must be less than 50 characters")]
         public string Name { get; set; }
 
         [Display(Name = "Address Line 1")]
-        [Required(ErrorMessage ="Street address is required")]
         [StringLength(100, ErrorMessage = "Must be less than 100 characters")]
+        [Required(ErrorMessage ="Street address is required")]
         public string AddressLine1 { get; set; }
 
         [Display(Name = "Address Line 2")]
@@ -25,8 +25,8 @@ namespace TexasPetroleum.ViewModels
         public string AddressLine2 { get; set; }
 
         [Display(Name = "City")]
-        [Required(ErrorMessage ="City is required")]
         [StringLength(100, ErrorMessage = "Must be less than 100 characters")]
+        [Required(ErrorMessage ="City is required")]
         public string City { get; set; }
 
         [Display(Name = "State")]
@@ -34,8 +34,8 @@ namespace TexasPetroleum.ViewModels
         public StateOptions StateOption { get; set; } = StateOptions.AK;
 
         [Display(Name = "Zipcode")]
-        [Required(ErrorMessage ="Zipcode is required")]
         [StringLength(9, MinimumLength = 5, ErrorMessage = "Must be between 5 and 9 characters in length")]
+        [Required(ErrorMessage ="Zipcode is required")]
         public string Zipcode { get; set; }
 
         public IEnumerable<SelectListItem> StateOptionsSelect { get; set; }
