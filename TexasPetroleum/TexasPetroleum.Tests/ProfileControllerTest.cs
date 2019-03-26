c64bc8b0e008dc72ca6fbe64c48b91811488e5f6
brizzetto <bobbyrizzetto@gmail.com>
2019-03-25 19:56:34 -0500

    Add Tests For Controllers

---
+++ TexasPetroleum/TexasPetroleum.Tests/ProfileControllerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using TexasPetroleum.Controllers;
+using Xunit;
+
+namespace TexasPetroleum.Tests
+{
+    public class ProfileControllerTest
+    {
+        [Fact]
+        public void Index_NotNull()
+        {
+            //Arrange
+             var controller = new ProfileController();
+
+            //Act
+            var result = controller.Index() as ViewResult;
+
+            //Assert
+            Assert.NotNull(result); 
+        }
+    }
+}
